ad53a9b0dc635560b98392fbb6e723ba20720241
Gustavo Gondim <gustavospgondim@gmail.com>
2020-06-09 14:34:10 -0300

    support for routes w same paths b/ different verbs

--- index.js
+++ index.js
@@ -44,13 +44,16 @@ class AzureMonofunction {
     this.router = null;
   }
 
+  // INTERNAL
   get routes() {
     return this.router.root;
   }
+  // INTERNAL
   set routes(value) {
     this.initializeRouter(value);
   }
 
+  // INTERNAL
   log(message, data) {
     if (this.debug) {
       const log = this.logger.log.bind({}, `[MONOFUNCTION] ${message}`);
@@ -58,30 +61,52 @@ class AzureMonofunction {
     }
   }
 
+  // INTERNAL
   initializeRouter(routeList) {
-    const options = {
+    // group all routes with same paths
+    const routeGroups = routeList.reduce((acc, route) => {
+      if (acc[route.path]) {
+        acc[route.path].routes.push(route);
+      } else {
+        acc[route.path] = { routes: [route] };
+      }
+      return acc;
+    }, {});
+
+    // maps a single route per path
+    const routes = Object.keys(routeGroups).map((path) => {
+      const group = routeGroups[path];
+
+      // action selects the route that matches the path and HTTP verb
+      const action = async (context) => {
+        const route = group.routes.find(r => this.matchAnyVerb(context, r));
+        if (route) {
+          return this.runAction(route, context);
+        }
+        context.res.status = 405;
+        throw new Error('405 Method not allowed');
+      };
+      return { path, action };
+    });
+
+    const routerOptions = {
       baseUrl: this.routePrefix,
       context: {
         middlewares: this.middlewares,
       },
-      errorHandler: this.error.bind(this),
+      errorHandler: this.runError.bind(this),
     };
-    const routes = routeList.map((route) => ({
-      path: route.path,
-      action: async (context) => this.action(route, context),
-    }));
-
     this.log('Initializing router with routes', routes);
-    this.router = new UniversalRouter(routes, options);
+    this.router = new UniversalRouter(routes, routerOptions);
   }
 
-  listen() {
-    const monofunction = this;
-    monofunction.log('Initializing function entrypoint');
-    return async (context) => monofunction.request(context);
+  // INTERNAL
+  matchAnyVerb(context, route) {
+    return !!route.methods.find(m => m.toUpperCase() === context.req.method.toUpperCase());
   }
 
-  async request(context) {
+  // INTERNAL
+  async executeRequest(context) {
     this.logger = context;
 
     this.log('Parsing URL', context.req.originalUrl);
@@ -108,8 +133,9 @@ class AzureMonofunction {
     return result;
   }
 
-  async action(route, context) {
-    if (!route.methods.find(m => m.toUpperCase() === context.req.method.toUpperCase())) {
+  // INTERNAL
+  async runAction(route, context) {
+    if (!this.matchAnyVerb(context, route)) {
       context.res.status = 405;
       throw new Error('405 Method not allowed');
     } else {
@@ -119,7 +145,7 @@ class AzureMonofunction {
 
       cascade.catch((error, context) => {
         monofunction.log('Triggered error inside cascade catch');
-        monofunction.error(error, context);
+        monofunction.runError(error, context);
       });
       cascade.pipeline.push(
         ...this.middlewares.preProcessingPipeline,
@@ -135,7 +161,8 @@ class AzureMonofunction {
     }
   }
 
-  error(error, context) {
+  // INTERNAL
+  runError(error, context) {
     if (error && error.status && error.status === 404 && this.notFoundHandler) {
       this.log('Triggered 404');
       return this.notFoundHandler(context);
@@ -144,24 +171,65 @@ class AzureMonofunction {
     if (this.errorHandler) this.errorHandler(error, context);
   }
 
+  /**
+   * Returns an Azure Function entrypoint for a HTTP Trigger.
+   * @returns Azure Function entrypoint for a HTTP Trigger `async (context) => {}`
+   * @memberof AzureMonofunction
+   */
+  listen() {
+    const monofunction = this;
+    monofunction.log('Initializing function entrypoint');
+    return async (context) => monofunction.executeRequest(context);
+  }
+
+  /**
+   * Registers an error handler to capture unexpected errors inside the function's middlewares.
+   * @param {Function} errorHandler Function with (error, context) arguments. 
+   * @memberof AzureMonofunction
+   */
   onError(errorHandler) {
     this.errorHandler = errorHandler;
   }
 
+  /**
+   * Registers a handler to be called when no route matches the request path.
+   * @param {Function} notFoundHandler Function with (context) argument.
+   * @memberof AzureMonofunction
+   */
   on404(notFoundHandler) {
     this.notFoundHandler = notFoundHandler;
   }
 
+  /**
+   * Adds a middleware for all routes.
+   * @param {AsyncGeneratorFunction} asyncFunction Middleware in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @param {('PRE_PROCESSING'|'MAIN'|'POST_PROCESSING')?} [phase='MAIN'] Phase to register the middleware as specified in Azure Functions Middlewares package.. Defaults to 'MAIN'.
+   * @returns
+   * @memberof AzureMonofunction
+   */
   use(asyncFunction, phase) {
     this.log('Adding global middleware');
     return this.middlewares.use(asyncFunction, phase);
   }
 
+  /**
+   * Adds a conditional middleware for all routes.
+   * @param {Function} expressionFunc Expression function in form of `(context) => {}` that returns a boolean, to validade if the middleware should run or not, as specified in Azure Functions Middlewares package..
+   * @param {AsyncGeneratorFunction} asyncFunction Middleware in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @param {('PRE_PROCESSING'|'MAIN'|'POST_PROCESSING')?} [phase='MAIN'] Phase to register the middleware as specified in Azure Functions Middlewares package.. Defaults to 'MAIN'.
+   * @returns
+   * @memberof AzureMonofunction
+   */
   useIf(expressionFunc, asyncFunction, phase) {
     this.log('Adding global conditional middleware');
     return this.middlewares.useIf(expressionFunc, asyncFunction, phase);
   }
 
+  /**
+   * Adds a list of routes to the monofunction router.
+   * @param {Array<{ path, methods, run, logger? }>} routes A list of valid routes.
+   * @memberof AzureMonofunction
+   */
   addRoutes(routes) {
     this.log('Adding routes middleware', routes);
     const routeList = Array.isArray(routes) ? routes : [routes];
@@ -175,17 +243,52 @@ class AzureMonofunction {
     });
   }
 
-  // route(methods, path, run) {
-  //   this.addRoutes({ methods, path, run });
-  // }
+  /**
+   * Adds a single route to the monofunction router.
+   * @param {Array<['GET','POST','PATCH','PUT','DELETE']>} methods List of HTTP verbs for the route.
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  route(methods, path, run) {
+    this.addRoutes({ methods, path, run });
+  }
 
-  // get(path, middlewares) { this.route(['GET'], path, middlewares); }
-  // post(path, middlewares) { this.route(['POST'], path, middlewares); }
-  // put(path, middlewares) { this.route(['PUT'], path, middlewares); }
-  // delete(path, middlewares) { this.route(['DELETE'], path, middlewares); }
-  // patch(path, middlewares) { this.route(['PATCH'], path, middlewares); }
-  // head(path, middlewares) { this.route(['HEAD'], path, middlewares); }
-  // options(path, middlewares) { this.route(['OPTIONS'], path, middlewares); }
+  /**
+   * Adds a single route to the monofunction router for HTTP verb "GET".
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  get(path, middlewares) { this.route(['GET'], path, middlewares); }
+  /**
+   * Adds a single route to the monofunction router for HTTP verb "POST".
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  post(path, middlewares) { this.route(['POST'], path, middlewares); }
+  /**
+   * Adds a single route to the monofunction router for HTTP verb "PUT".
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  put(path, middlewares) { this.route(['PUT'], path, middlewares); }
+  /**
+   * Adds a single route to the monofunction router for HTTP verb "DELETE".
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  delete(path, middlewares) { this.route(['DELETE'], path, middlewares); }
+  /**
+   * Adds a single route to the monofunction router for HTTP verb "PATCH".
+   * @param {string} path The URL path for the route.
+   * @param {AsyncGeneratorFunction|AsyncGeneratorFunction[]} run A single function middleware or a list of middlewares tha should run for the route, in form of `async (context) => {}` as specified in Azure Functions Middlewares package.
+   * @memberof AzureMonofunction
+   */
+  patch(path, middlewares) { this.route(['PATCH'], path, middlewares); }
 }
 
 module.exports = new AzureMonofunction(routePrefix);
